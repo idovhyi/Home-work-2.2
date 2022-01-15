e28b2859cb2e50e0090b927c36bc62f2a0fcc7ce
Ihor Dovhyi <dovhyy@gmail.com>
2022-01-15 20:56:44 +0200

    Initial Commit

---
+++ Home work 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2.2
+//
+//  Created by Ihor Dovhyi on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
